9fa012f81ef31435b5bea867a1408bb2529292ba
Greg <schergr@users.noreply.github.com>
2025-07-02 17:38:18 -0400

    chore: remove unused imports

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,10 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_httpauth import HTTPBasicAuth
 from flask_cors import CORS
 from googleapiclient.discovery import build
 from google.oauth2.service_account import Credentials
 from sqlalchemy.exc import OperationalError
-import json
 import time
 
 try:
