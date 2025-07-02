4ae7351186c165cd9a535891c7c7260a2becaa78
Greg <schergr@users.noreply.github.com>
2025-07-02 18:00:18 -0400

    Add MUI DataGrid integration

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/globals.css';
 import Nav from '../components/Nav';
+import { CssBaseline } from '@mui/material';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <CssBaseline />
       <Nav />
       <Component {...pageProps} />
     </>
